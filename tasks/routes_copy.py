0036e33467ba0b03bbab790065f8bab2d5dc61fd
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2019-12-20 00:55:35 +0100

    Actualización inicial

--- tasks/routes_copy.py
+++ tasks/routes_copy.py
@@ -1,13 +1,19 @@
 from tasks import app
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 
 import csv
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
+    <lee tareas en fichero .data/tareas.dat>
+    return render_template("index.html", tareas)
+
+@app.route("/newtask", methods['GET', 'POST'])
+def newtask():
     if request.method == 'GET':
-        return render_template("task.html")
+        return render_template('task.html')
+
 
     fdatos = open('./data/tareas.dat', 'w')
     csvwriter = csv.writer(fdatos, delimiter=",", quotechar='"')
@@ -19,4 +25,4 @@ def index():
     csvwriter.writerow([title, desc, date])
 
     fdatos.close()
-    return render_template("task.html")
+    return redirect(url_for("index"))
