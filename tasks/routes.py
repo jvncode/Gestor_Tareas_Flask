68c32baef4bbf852beb8de3ac266a8169935f014
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2019-12-24 02:07:25 +0100

    Reto de Añadir Tareas alcanzado

--- tasks/routes.py
+++ tasks/routes.py
@@ -1,22 +1,34 @@
 from tasks import app
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 
 import csv
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    if request.method == 'GET':
-        return render_template("task.html")
+    fdatos = open('./data/tareas.dat','r')
+    csvreader = csv.reader(fdatos, delimiter=',')
+    tareas = []
+    for campo in csvreader:
+        tareas.append(campo)
+    fdatos.close()
+    return render_template("index.html", registros=tareas)
 
-    fdatos = open('./data/tareas.dat', 'w')
+@app.route("/newtask", methods=['GET', 'POST'])
+def newtask():
+    if request.method == 'GET':
+        return render_template('tasks.html')
+
+    fdatos = open('./data/tareas.dat', 'a')
     csvwriter = csv.writer(fdatos, delimiter=",", quotechar='"')
 
+    
     title = request.values.get('title')
     desc = request.values.get('desc')
     date = request.values.get('date')
 
-    csvwriter.writerow([title, desc, date])
+    if title!="" and desc!="" and date!="":
+        csvwriter.writerow([title, desc, date])
 
     fdatos.close()
-    return render_template("task.html")
+    return redirect(url_for("index"))
