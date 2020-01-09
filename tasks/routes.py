614729c56d829f5c4d340ad469b5fad34d0d18b9
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2020-01-10 00:51:32 +0100

    Completadas funciones a falta de conexión con BBDD

--- tasks/routes.py
+++ tasks/routes.py
@@ -1,13 +1,17 @@
 from tasks import app
 from flask import render_template, request, url_for, redirect
-from tasks.forms import TaskForm
+from tasks.forms import TaskForm, ProccesTaskForm
 
-import csv
+import csv, sqlite3
+from datetime import date
 
+DATOS = './data/tareas.dat'
+COPIA = './data/copia.txt'
+BASE_DATOS = './data/task.db'
 
 @app.route("/")
 def index():
-    fdatos = open('./data/tareas.dat','r')
+    fdatos = open(DATOS,'r')
     csvreader = csv.reader(fdatos, delimiter=',', quotechar='"')
     tareas = []
     for campo in csvreader:
@@ -23,7 +27,7 @@ def newtask():
         return render_template('tasks.html', form=form)
 
     if form.validate():
-        fdatos = open('./data/tareas.dat', 'a')
+        fdatos = open(DATOS, 'a')
         csvwriter = csv.writer(fdatos, delimiter=",", quotechar='"')
 
         title = request.values.get('title')
@@ -36,3 +40,57 @@ def newtask():
         return redirect(url_for("index"))
     else:
         return render_template("tasks.html", form=form)
+
+@app.route("/processtask", methods=['GET', 'POST'])
+def processtask():
+    form = ProccesTaskForm(request.form)
+
+    if request.method == 'GET':
+
+        fdatos = open(DATOS,'r')
+        csvreader = csv.reader(fdatos, delimiter=',', quotechar='"')
+
+        registroAct = None
+        ilinea = 1
+        ix = int(request.values.get('ix'))
+        for linea in csvreader:
+            if ilinea == ix:
+                registroAct = linea
+                break
+            ilinea += 1
+
+        if registroAct:
+            if registroAct[2]:
+                fechaTarea = date(int(registroAct[2][:4]), int(registroAct[2][5:7]), int(registroAct[2][8:]))
+            else:
+                fechaTarea: None
+            
+            accion = " "
+            if 'btnModificar' in request.values:
+                accion = 'M'
+            
+            if 'btnBorrar' in request.values:
+                accion = 'B'
+            form = ProccesTaskForm(data={'ix': ix, 'title': registroAct[0], 'description': registroAct[1], 'date': fechaTarea, 'btn': accion})
+
+        return render_template('processtask.html', form=form)
+
+    if form.btn.data == 'B':
+        print('Borrar registro')
+        return redirect(url_for('index'))
+
+    if form.btn.data == 'M':
+        if form.validate():
+            print("Modificar el fichero")
+            '''
+            Crear fichero copia vacio en escritura
+            leer y copiar todos los registros desde tareas.txt a copia.txt hasta el anterior al que vamos a modificar
+            grabar el nuevo registro con los datos del formulario
+            leer y copiar el resto de los registros hasta el final
+            cerrar los dos ficheros
+            borrar tareas.txt
+            renombrar copia.txt a tareas.txt
+            '''
+            return redirect(url_for('index'))
+
+        return render_template("processtask.html", form=form)
